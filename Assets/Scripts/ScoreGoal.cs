0a74827dbc95d0543f425717986fdc985398f869
Alia ElKattan <aae322@nyu.edu>
2019-10-02 19:30:34 +0400

    uploaded project

---
+++ Assets/Scripts/ScoreGoal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreGoal : MonoBehaviour {
+    private int count1;
+   
+      // Use this for initialization
+   void Start () {
+        count1 = 0;
+	}
+	/*
+	// Update is called once per frame
+	void Update () {
+		
+	}*/
+
+      
+    void OnCollisionEnter(Collision col)
+    {
+       if (col.gameObject.name == "puck")
+        {
+           print("collided!");
+            col.gameObject.transform.Translate(new Vector3(0, 0, 2.25f));
+            count1++;
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/ScoreGoal2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreGoal2 : MonoBehaviour
+{
+    private int count2;
+
+    // Use this for initialization
+     void Start () {
+        count2 = 0;
+     }
+
+        /*
+
+     // Update is called once per frame
+     void Update () {
+
+     }*/
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "puck")
+        {
+            print("collided!");
+            col.gameObject.transform.Translate(new Vector3(0, 0, -2.3f));
+            col.gameObject.transform.Translate(new Vector3(0, 0, 0));
+            count2++;
+        }
+    }
+}
+
+
